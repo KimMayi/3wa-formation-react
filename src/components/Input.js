444a4ad54b6fb36f3c8508fa25005ff0625f8a66
Kim <kim.bellesort@3wa.io>
2023-11-27 11:56:07 +0100

    first commit

---
+++ src/components/Input.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Input(props) {
+
+return (
+        
+<div>   
+    <input 
+        onChange={props}
+        name="Bill"  
+        value={props}
+        type= "text" 
+        placeholder= "Enter your bill"/>
+        
+ </div>
+    
+)}
